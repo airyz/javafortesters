f0222ed3de80ad15dd4bb84ded1bf97f57cdd161
airyz <eraclis@gmail.com>
2016-09-01 16:42:58 +0300

    test commit

---
+++ src/test/java/com/javafortesters/chap003myfirsttest/examples/MyFirstTest.java
@@ -0,0 +1,22 @@
+/**
+ * Created by eraclis on 30/8/2016.
+ */
+
+package com.javafortesters.chap003myfirsttest.examples;
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class MyFirstTest {
+
+    @Test
+    public void canAddTwoPlusTwo() {
+        int answer = 2+2;
+        assertEquals("2+2=4", 4, answer);
+
+
+    }
+}
